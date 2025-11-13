087bb3a08da1af37c89b26d6104ef5a7c0767047
Souvik <souvik.sural@capsitech.com>
2025-11-13 19:43:12 +0530

    Vehicle choose Panel

---
+++ Assets/Script/VehicleSelector.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class VehicleSelector : MonoBehaviour
+{
+    [Header("Vehicles")]
+    public GameObject[] vehicles;
+    private int currentIndex = 0;
+    private Vector3 spawnPosition;
+
+    [Header("UI Elements")]
+    public GameObject selectionPanel;
+    public Button leftButton;
+    public Button rightButton;
+    public Button playButton;
+
+    void Start()
+    {
+        
+        spawnPosition = vehicles[0].transform.position;
+
+     
+        foreach (GameObject v in vehicles)
+            v.SetActive(false);
+
+      
+        ShowVehicle(currentIndex);
+
+        
+        leftButton.onClick.AddListener(PrevVehicle);
+        rightButton.onClick.AddListener(NextVehicle);
+        playButton.onClick.AddListener(StartGame);
+    }
+
+    void ShowVehicle(int index)
+    {
+        
+        foreach (GameObject v in vehicles)
+            v.SetActive(false);
+
+        
+        vehicles[index].transform.position = spawnPosition;
+        vehicles[index].SetActive(true);
+    }
+
+    public void NextVehicle()
+    {
+        currentIndex++;
+        if (currentIndex >= vehicles.Length)
+            currentIndex = 0;
+        ShowVehicle(currentIndex);
+    }
+
+    public void PrevVehicle()
+    {
+        currentIndex--;
+        if (currentIndex < 0)
+            currentIndex = vehicles.Length - 1;
+        ShowVehicle(currentIndex);
+    }
+
+    public void StartGame()
+    {
+       
+        selectionPanel.SetActive(false);
+
+      
+        var driver = vehicles[currentIndex].GetComponent<Driver>();
+        if (driver != null)
+            driver.enabled = true;
+    }
+}
